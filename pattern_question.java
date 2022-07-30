4f2af064ed583d490829fb8f8bd44a406e43f6aa
jay <jaydobariya2341@gmail.com>
2022-07-30 12:31:21 +0530

    10 Pattern question

--- pattern_question.java
+++ pattern_question.java
@@ -1,3 +1,5 @@
+import javax.print.event.PrintEvent;
+
 public class pattern_question {
     public static void main(String[] args) {
         
@@ -7,7 +9,7 @@ public class pattern_question {
         for (int j = 1; j <=4 ; j++) {
             System.out.print("* ");
         }
-        System.out.println("");
+        System.out.println();
     }
     
 
@@ -58,6 +60,82 @@ public class pattern_question {
     }
 
 
+    // ----------------------------------- Pattern-6 -----------------------------------     
+    System.out.println("Pattern-6");
+    for (int i = 1; i <=5; i++) {
+        for (int j = 1; j <=i; j++) {
+            System.out.print(j+" ");
+        }
+        System.out.println();
+    }
+
+
+     // ----------------------------------- Pattern-7 -----------------------------------     
+     System.out.println("Pattern-7");
+    for (int i = 1; i<=5 ; i++) {
+        for (int j = 1; j<=5-i+1; j++) {
+            System.out.print(j+" ");
+        }
+        System.out.println();
+    }
+
+
+
+     // ----------------------------------- Pattern-8 -----------------------------------     
+     System.out.println("Pattern-8");
+     int number =1;
+    for (int i = 1; i <=5; i++) {
+        for (int j = 1; j<=i; j++) {
+            System.out.print(number+" ");
+            number++;
+        } 
+        System.out.println();
+    }
+
+    
+     // ----------------------------------- Pattern-9 -----------------------------------     
+     System.out.println("Pattern-9");
+    for (int i = 1; i<=5 ; i++) {
+        for (int j = 1; j<=i; j++) {
+            int sum = i+j;
+            if (sum % 2 == 0) {
+                System.out.print("1 ");
+            } else {
+                System.out.print("0 ");
+            }
+        }
+        System.out.println();
+    }
+
+
+     // ----------------------------------- Pattern-10 -----------------------------------     
+     System.out.println("Pattern-10");
+    //  first half 
+    for (int i = 1; i<=4; i++) {
+        for (int j = 1; j <=i; j++) {
+        System.out.print("*");
+        }
+        for (int j = 1; j <=2*(4-i); j++) {
+        System.out.print(" ");
+        }
+        for (int j = 1; j <=i; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    // second half 
+    for (int i = 4; i>=1; i--) {
+        for (int j = 1; j <=i; j++) {
+        System.out.print("*");
+        }
+        for (int j = 1; j <=2*(4-i); j++) {
+        System.out.print(" ");
+        }
+        for (int j = 1; j <=i; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 
 
 }
