48904be169e954acc4475fdd1bceb19426371836
jay <jaydobariya2341@gmail.com>
2022-07-30 17:03:42 +0530

    Done all Example given by apna collage

--- pattern_question.java
+++ pattern_question.java
@@ -138,5 +138,74 @@ public class pattern_question {
     }
 
 
+     // ----------------------------------- Pattern-11 -----------------------------------     
+     System.out.println("Pattern-11");
+    for (int i = 1; i <=5 ; i++) {
+        for (int j = 0; j<5-i+1; j++) {
+            System.out.print(" ");
+        }
+        for (int j = 1; j <=5; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+
+    // ----------------------------------- Pattern-11 -----------------------------------     
+    System.out.println("Pattern-11");
+    for (int i = 1; i <=5; i++) {
+        for (int j = 1; j <=5-i ; j++) {
+            System.out.print(" ");
+        }
+        for (int j = 1; j<=i ; j++) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+
+
+// ----------------------------------- Pattern-12 -----------------------------------     
+System.out.println("Pattern-12");
+for (int i = 1; i <=5; i++) {
+    for (int j = 1; j <=5-i; j++) {
+        System.out.print("  ");
+    }
+    for (int j = i; j >=1 ; j--) {
+        System.out.print (j+" ");
+    }
+    for (int j = 2; j<=i ; j++) {
+        System.out.print(j+" ");
+    }
+ 
+    System.out.println();
+}
+
+
+// ----------------------------------- Pattern-13 -----------------------------------     
+System.out.println("Pattern-13");
+for (int i = 1; i <=4; i++) {
+    for (int j = 1; j <=4-i; j++) {
+        System.out.print("  ");
+    }
+    for (int j = 1; j <=2*i-1; j++) {
+        System.out.print("* ");
+    }
+    System.out.println();
+}
+for (int i = 4; i >=1; i--) {
+    for (int j = 1; j <=4-i; j++) {
+        System.out.print("  ");
+    }
+    for (int j = 1; j <=2*i-1; j++) {
+        System.out.print("* ");
+    }
+    System.out.println();
+}
+
+
+
+
+
+
 }
 }
