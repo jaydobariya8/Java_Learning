e520859552099bcc24c843958f451c1d2586df25
jay <jaydobariya2341@gmail.com>
2022-08-01 11:23:41 +0530

    until 2d array example

--- array.java
+++ array.java
@@ -1,5 +1,7 @@
 public class array {
     public static void main(String[] args) {
+
+        // what is array : array is collection of an similar type of datatype 
         
         // There are three method to create an array
         
@@ -24,7 +26,7 @@ public class array {
         int [] marks ={40,78,86,95,45};
 
         // find an length of an array 
-        System.out.println("Length of an array is: "+ marks.length);
+        // System.out.println("Length of an array is: "+ marks.length);
         
         // System.out.println(marks[0]);
         // System.out.println(marks[1]);
@@ -40,8 +42,31 @@ public class array {
         // }
         
         // using for-each loop 
-        for (int i : marks) {
-            System.out.println(i);
+        // for (int i : marks) {
+        //     System.out.println(i);
+        // }
+
+
+        // multi dimantional array: array od array also called 2D array 
+        int [][] flate = new int[2][3];
+        flate[0][0] = 101;
+        flate[0][1] = 102;
+        flate[0][2] = 103;
+        flate[1][0] = 201;
+        flate[1][1] = 202;
+        flate[1][2] = 203;
+        
+        for (int i = 0; i < flate.length; i++) {
+            for (int j = 0; j < flate[i].length; j++) {
+                System.out.print(flate[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
         }
+
+
+
+
+
     }
 }
