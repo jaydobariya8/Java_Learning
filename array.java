d2df6a5c7f68e92bf192bdc73ad6509985c79fdd
jay <jaydobariya2341@gmail.com>
2022-08-01 10:40:53 +0530

    basic of array

---
+++ array.java
@@ -0,0 +1,43 @@
+public class array {
+    public static void main(String[] args) {
+        
+        // There are three method to create an array
+        
+        // 1- method to create an array
+
+        // int[] marks;
+        // marks = new int [2];
+        // marks[0]=56;
+        // marks[1]=67;
+        // marks[2]=76;
+
+        // 2- method to create an array
+
+        // int[] marks = new int [2];
+        // marks[0]=56;
+        // marks[1]=67;
+        // marks[2]=76;
+
+
+        // 3- method to create an array
+
+        int [] marks ={40,78,86,95,45};
+        
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        // for (int i = 0; i < marks.length; i++) {
+        //     System.out.println(marks[i]);
+        // }
+
+        // for (int i = marks.length-1; i>=1; i--) {
+        //     System.out.println(marks[i]);
+        // }
+        
+        // using for-each loop 
+        for (int i : marks) {
+            System.out.println(i);
+        }
+    }
+}
