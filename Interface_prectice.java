16b3d8cb6750a6b458b2675bbfda356db1266884
jay <jaydobariya2341@gmail.com>
2022-08-08 19:08:25 +0530

    interface

--- Interface_prectice.java
+++ Interface_prectice.java
@@ -1,57 +1,56 @@
-interface MyCamera{
-    void takeSnap();
-    void recordVideo();
-    private void greet(){
-        System.out.println("Good Morning");
-    }
-    default void record4KVideo(){
-        greet();
-        System.out.println("Recording in 4k...");
-    }
-}
+// interface MyCamera{
+//     void takeSnap();
+//     void recordVideo();
+//     private void greet(){
+//         System.out.println("Good Morning");
+//     }
+//     default void record4KVideo(){
+//         greet();
+//         System.out.println("Recording in 4k...");
+//     }
+// }
 
-interface MyWifi{
-    String[] getNetworks();
-    void connectToNetwork(String network);
-}
+// interface MyWifi{
+//     String[] getNetworks();
+//     void connectToNetwork(String network);
+// }
 
-class MyCellPhone{
-    void callNumber(int phoneNumber){
-        System.out.println("Calling "+ phoneNumber);
-    }
-    void pickCall(){
-        System.out.println("Connecting... ");
-    }
+// class MyCellPhone{
+//     void callNumber(int phoneNumber){
+//         System.out.println("Calling "+ phoneNumber);
+//     }
+//     void pickCall(){
+//         System.out.println("Connecting... ");
+//     }
 
-}
+// }
 
-class MySmartPhone extends MyCellPhone implements MyWifi, MyCamera{
-    public void takeSnap(){
-        System.out.println("Taking snap");
-    }
-    public void recordVideo(){
-        System.out.println("Taking snap");
-    }
-//    public void record4KVideo(){
-//        System.out.println("Taking snap and recoding in 4k");
-//    }
-    public String[] getNetworks(){
-        System.out.println("Getting List of Networks");
-        String[] networkList = {"Harry", "Prashanth", "Anjali5G"};
-        return networkList;
-    }
-    public void connectToNetwork(String network){
-        System.out.println("Connecting to " + network);
-    }
+// class MySmartPhone extends MyCellPhone implements MyWifi, MyCamera{
+//     public void takeSnap(){
+//         System.out.println("Taking snap");
+//     }
+//     public void recordVideo(){
+//         System.out.println("Taking snap");
+//     }
+// //    public void record4KVideo(){
+// //        System.out.println("Taking snap and recoding in 4k");
+// //    }
+//     public String[] getNetworks(){
+//         System.out.println("Getting List of Networks");
+//         String[] networkList = {"Harry", "Prashanth", "Anjali5G"};
+//         return networkList;
+//     }
+//     public void connectToNetwork(String network){
+//         System.out.println("Connecting to " + network);
+//     }
     
-}
+// }
 
-public class Interface_prectice {
-    MySmartPhone asus = new MySmartPhone();
-    String[] arr = asus.getNetworks();
-    for(String element: arr){
-        System.out.println(element);
-    }
-
-    
-}
+// public class Interface_prectice {
+//     MySmartPhone ms = new MySmartPhone();
+//     ms.takeSnap();
+//     String[] arr = ms.getNetworks();
+//     for(String element: arr){
+//         System.out.println(element);
+//     }
+// }
