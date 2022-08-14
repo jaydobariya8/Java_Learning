1f1b41c1ac6937598ecbbcf8310208a993130499
jay <jaydobariya2341@gmail.com>
2022-08-14 12:57:29 +0530

    runnneble edit

--- thread_runneble.java
+++ thread_runneble.java
@@ -1,32 +1,32 @@
-class thred1 implements Runnable{
-    public void run(){
-        int i = 0;
-    while(i<=400){
-        System.out.println("i am working on java");
-        i++;
-    }
-    }
+// class thred1 implements Runnable{
+//     public void run(){
+//         int i = 0;
+//     while(i<=400){
+//         System.out.println("i am working on java");
+//         i++;
+//     }
+//     }
    
-}
-class thred2 implements Runnable{
-    public void run(){
-        int i = 0;
-        while(i<=400){
-            System.out.println("happy with DSA");
-            i++;
-        }
-    }
-}
+// }
+// class thred2 implements Runnable{
+//     public void run(){
+//         int i = 0;
+//         while(i<=400){
+//             System.out.println("happy with DSA");
+//             i++;
+//         }
+//     }
+// }
 
-public class thread_runneble {
-    public static void main(String[] args) {
-        thred1 t1 = new thred1();
-        Thread th1 = new Thread(t1);
+// public class thread_runneble {
+//     public static void main(String[] args) {
+//         thred1 t1 = new thred1();
+//         Thread th1 = new Thread(t1);
 
-        thred2 t2 = new thred2();
-        Thread th2 = new Thread(t2);
+//         thred2 t2 = new thred2();
+//         Thread th2 = new Thread(t2);
 
-        th1.start();
-        th2.start();
-    }
-}
+//         th1.start();
+//         th2.start();
+//     }
+// }
